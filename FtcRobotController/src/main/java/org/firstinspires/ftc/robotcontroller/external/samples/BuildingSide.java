063a5e5ff62ea56371bbc12118a97fa57de325f1
RohanR64 <rohanvr03@gmail.com>
2019-12-14 09:19:00 -0700

    today

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BuildingSide.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BuildingSide.java
@@ -6,20 +6,24 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous
 public class BuildingSide extends LinearOpMode {
     private DcMotor leftWheel;
     private DcMotor rightWheel;
+    private Servo clawServo;
 
     @Override
     public void runOpMode() {
         ElapsedTime opmodeRunTime = new ElapsedTime();
         leftWheel = hardwareMap.get(DcMotor.class, "leftWheel");
         rightWheel = hardwareMap.get(DcMotor.class, "rightWheel");
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
         telemetry.addData("Status", "Initialized ");
         telemetry.update();
         double wheelPowerTarget = 0;
+        double armRaisedState = 0; //arm is in neutral state at the start
+        int armPositionTarget =0;
         wheelPowerTarget = 1;
         opmodeRunTime.reset();
         while (opmodeRunTime.seconds() < 1.0) {
