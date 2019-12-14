8deb2b3ff80913d8617d582a6e0fcec60cd78727
Epicamoch <41214739+Epicamoch@users.noreply.github.com>
2019-12-14 09:51:31 -0700

    Banana

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BuildingSide.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BuildingSide.java
@@ -3,47 +3,67 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
-@Autonomous
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
 public class BuildingSide extends LinearOpMode {
-    private DcMotor leftWheel;
-    private DcMotor rightWheel;
-    private Servo clawServo;
+    private DcMotor leftWheel = hardwareMap.get(DcMotor.class, "leftWheel");
+    private DcMotor rightWheel = hardwareMap.get(DcMotor.class, "rightWheel");
+    private DcMotor armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+    private Servo clawServo = hardwareMap.get(Servo.class, "clawServo");
 
     @Override
     public void runOpMode() {
         ElapsedTime opmodeRunTime = new ElapsedTime();
-        leftWheel = hardwareMap.get(DcMotor.class, "leftWheel");
-        rightWheel = hardwareMap.get(DcMotor.class, "rightWheel");
-        clawServo = hardwareMap.get(Servo.class, "clawServo");
-        telemetry.addData("Status", "Initialized ");
+        telemetry.addData("Status", "Initialized");
         telemetry.update();
-        double wheelPowerTarget = 0;
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        // run until the end of the match (driver presses STOP)
         double armRaisedState = 0; //arm is in neutral state at the start
-        int armPositionTarget =0;
-        wheelPowerTarget = 1;
+        int armPositionTarget = 0;
         opmodeRunTime.reset();
-        while (opmodeRunTime.seconds() < 1.0) {
-            rightWheel.setPower(wheelPowerTarget);
-            telemetry.update();
+        while (opmodeRunTime.seconds() < 1.0){
+            driveForward(1);
         }
-        opmodeRunTime.reset();
-        while (opmodeRunTime.seconds() < 3.0) {
-            rightWheel.setPower(-wheelPowerTarget);
-            leftWheel.setPower(wheelPowerTarget);
-            telemetry.update();
-        }
-        opmodeRunTime.reset();
-        while (opmodeRunTime.seconds() < 1.0) {
-            leftWheel.setPower(wheelPowerTarget);
-            telemetry.update();
-        }
-        wheelPowerTarget = 0;
-        rightWheel.setPower(-wheelPowerTarget);
-        leftWheel.setPower(wheelPowerTarget);
+    }
+
+    public void driveForward(double power){
+        leftWheel.setPower(power);
+        rightWheel.setPower(-power);
+        telemetry.addData("Left Wheel Power", leftWheel.getPower());
+        telemetry.addData("Right Wheel Power", rightWheel.getPower());
+        telemetry.addData("is Moving", "Forward");
+        telemetry.update();
+    }
+    public void driveBackward(double power){
+        leftWheel.setPower(-power);
+        rightWheel.setPower(power);
+        telemetry.addData("Left Wheel Power", leftWheel.getPower());
+        telemetry.addData("Right Wheel Power", rightWheel.getPower());
+        telemetry.addData("is Moving", "Backward");
+        telemetry.update();
+    }
+    public void turnLeft(double power){
+        leftWheel.setPower(-power);
+        rightWheel.setPower(-power);
+        telemetry.addData("Left Wheel Power", leftWheel.getPower());
+        telemetry.addData("Right Wheel Power", rightWheel.getPower());
+        telemetry.addData("is Turning", "Left");
+        telemetry.update();
+    }
+    public void turnRight(double power){
+        leftWheel.setPower(power);
+        rightWheel.setPower(power);
+        telemetry.addData("Left Wheel Power", leftWheel.getPower());
+        telemetry.addData("Right Wheel Power", rightWheel.getPower());
+        telemetry.addData("is Turning", "Right");
+        telemetry.update();
+    }
+    public void closeGrab(){
 
     }
 }
